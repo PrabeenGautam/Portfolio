4e8e3b1f74cc9610e6e6f8ff2b56c64ca4054ace
PrabinGautam <prabeen122@gmail.com>
2022-11-21 20:09:27 +0545

    IsoTope Added

---
+++ js/library.js
@@ -0,0 +1,34 @@
+var iso = new Isotope(".grid", {
+  itemSelector: ".project-item",
+  layoutMode: "fitRows",
+});
+
+// filter functions
+var filterFns = {
+  // show if name ends with -ium
+  ium: function (itemElem) {
+    var name = itemElem.querySelector(".name").textContent;
+    return name.match(/ium$/);
+  },
+};
+
+// bind filter button click
+var filtersElem = document.querySelector(".filters-button-group");
+filtersElem.addEventListener("click", function (event) {
+  if (!matchesSelector(event.target, ".filter-list")) return;
+  var filterValue = event.target.getAttribute("data-filter");
+  filterValue = filterFns[filterValue] || filterValue;
+  iso.arrange({ filter: filterValue });
+});
+
+// change active class on list
+var filterGroups = document.querySelector(".filter-group");
+radioButtonGroup(filterGroups);
+
+function radioButtonGroup(filterGroup) {
+  filterGroup.addEventListener("click", function (event) {
+    if (!matchesSelector(event.target, ".filter-list")) return;
+    filterGroup.querySelector(".active").classList.remove("active");
+    event.target.classList.add("active");
+  });
+}
--- js/script.js
+++ js/script.js
@@ -4,15 +4,6 @@ const tabsContent = document.querySelectorAll(".tab-content");
 const scrollUp = document.querySelector(".scroll-up");
 const themeSwitcher = document.querySelector(".theme-switcher");
 const headerSections = document.querySelector(".header");
-const sectionContainer = document.querySelectorAll(".section-counter");
-
-/* Side Nav */
-const sideNavItems = document.querySelectorAll(".nav-icons > a");
-const sideNavContainer = document.querySelector(".side-nav");
-
-/* Side Nav */
-const mainNavItems = document.querySelectorAll(".nav_link");
-const mainNavContainer = document.querySelector(".nav-container");
 
 toggleButtons.addEventListener("click", function (e) {
   if (!e.target.closest(".tabs")) return;
@@ -31,7 +22,6 @@ const headerObserver = new ObserverCreator(
   (entries) => {
     entries.forEach((entry) => {
       scrollUp.classList.toggle("show", !entry.isIntersecting);
-      sideNavContainer.classList.toggle("show", !entry.isIntersecting);
     });
   },
   {
@@ -44,27 +34,6 @@ headerObserver.observeElement();
 scrollUp.addEventListener("click", function (e) {
   window.scrollTo(0, 0);
 });
-
-const sectionCallback = (entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      const href = entry.target.getAttribute("id");
-      sideNavItems.forEach((element) => {
-        const parentElement = element.closest(".side-nav-items");
-        parentElement.classList.toggle(
-          "active",
-          element.getAttribute("href").includes(href)
-        );
-      });
-    }
-  });
-};
-
-const sectionObserver = new ObserverCreator(sectionContainer, sectionCallback, {
-  threshold: 1,
-});
-sectionObserver.createObserver();
-sectionObserver.observeElement();
 
 const sunSVG = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sun" viewBox="0 0 16 16">
   <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
